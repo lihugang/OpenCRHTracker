5381269fc5ac4d754224722f90fdfab9b16fb8b2
lihugang <lihugang@outlook.com>
2026-03-12 19:13:42 +0800

    chore: move daily emu route migration to script

--- server/libs/database/emu.ts
+++ server/libs/database/emu.ts
@@ -4,36 +4,10 @@ import useDatabase, {
 } from '~/server/libs/database/common';
 import importSqlBatch from '~/server/utils/sql/importSqlBatch';
 
-interface DailyEmuRoutesColumnRow {
-    name: string;
-}
-
-type EmuSchemaSqlKey =
-    | 'alterDailyEmuRoutesAddEndStationName'
-    | 'alterDailyEmuRoutesAddStartStationName'
-    | 'createDailyEmuRoutesTable'
-    | 'createProbeStatusTable';
-
-type EmuMetaSqlKey = 'selectDailyEmuRoutesColumns';
-
 function ensureEmuTrackedSchema(db: Database.Database) {
-    const schemaSql = importSqlBatch('emu/schema') as Record<
-        EmuSchemaSqlKey,
-        string
-    >;
-    const metaSql = importSqlBatch('emu/meta') as Record<EmuMetaSqlKey, string>;
-
-    db.exec(schemaSql.createDailyEmuRoutesTable);
-    db.exec(schemaSql.createProbeStatusTable);
-
-    const columnRows = db.prepare(metaSql.selectDailyEmuRoutesColumns)
-        .all() as DailyEmuRoutesColumnRow[];
-    const columns = new Set(columnRows.map((row) => row.name));
-    if (!columns.has('start_station_name')) {
-        db.exec(schemaSql.alterDailyEmuRoutesAddStartStationName);
-    }
-    if (!columns.has('end_station_name')) {
-        db.exec(schemaSql.alterDailyEmuRoutesAddEndStationName);
+    const schemaSql = importSqlBatch('emu/schema');
+    for (const statement of Object.values(schemaSql)) {
+        db.exec(statement);
     }
 }
 
