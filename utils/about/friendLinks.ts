69867b11a45ce6e7b526171c90a512370b6d61c1
lihugang <lihugang@outlook.com>
2026-06-20 23:56:14 +0800

    docs: add friend links

--- utils/about/friendLinks.ts
+++ utils/about/friendLinks.ts
@@ -16,5 +16,17 @@ export const aboutFriendLinks: AboutFriendLinkDefinition[] = [
     {
         name: '中国铁路地图数据下载',
         url: 'https://crmapdata.lihugang.top'
+    },
+    {
+        name: '动车组配属查询',
+        url: 'https://allocation.crhdata.top'
+    },
+    {
+        name: '铁路乘车提示',
+        url: 'https://p.crhdata.top'
+    },
+    {
+        name: '列车运行图在线查看',
+        url: 'https://paiagram.com/demo'
     }
 ];
